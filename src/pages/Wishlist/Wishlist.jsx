fde95180d3e5efd2c1e214c75be1faa8d983a8a1
[Aman Jain] <[ajain8479@gmail.com]>
2022-03-20 12:55:57 +0530

    added signup page with jsx and alter class to classname

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -7,113 +7,113 @@ const Wishlist = () => {
   return (
     <div>
       <Navbar />
-      <h3 class="main-heading">My Wishlist</h3>
+      <h3 className="main-heading">My Wishlist</h3>
       <main className="wishlist-main">
-        <section class="wishlist-product-menu">
-          <div class="card ecom-card">
+        <section className="wishlist-product-menu">
+          <div className="card ecom-card">
             <img src={productImage} alt="Product Image" />
 
-            <p class="wishlist-icon">
+            <p className="wishlist-icon">
               <ion-icon name="heart"></ion-icon>
             </p>
 
-            <p class="manufacturer">Demo Product</p>
+            <p className="manufacturer">Demo Product</p>
 
-            <p class="price">
+            <p className="price">
               ₹ 449
-              <span class="mrp">₹ 899</span>
-              <span class="discount">(49% off)</span>
+              <span className="mrp">₹ 899</span>
+              <span className="discount">(49% off)</span>
             </p>
 
-            <div class="action-container">
-              <button class="btn">Move to Cart</button>
+            <div className="action-container">
+              <button className="btn">Move to Cart</button>
 
-              <button class="btn btn-tertiary">Remove from Wishlist</button>
+              <button className="btn btn-tertiary">Remove from Wishlist</button>
             </div>
           </div>
 
-          <div class="card ecom-card">
+          <div className="card ecom-card">
             <img src={productImage} alt="Product Image" />
 
-            <p class="wishlist-icon">
+            <p className="wishlist-icon">
               <ion-icon name="heart"></ion-icon>
             </p>
 
-            <p class="manufacturer">Demo Product</p>
+            <p className="manufacturer">Demo Product</p>
 
-            <p class="price">
+            <p className="price">
               ₹ 449
-              <span class="mrp">₹ 899</span>
-              <span class="discount">(49% off)</span>
+              <span className="mrp">₹ 899</span>
+              <span className="discount">(49% off)</span>
             </p>
 
-            <div class="action-container">
-              <button class="btn">Move to Cart</button>
+            <div className="action-container">
+              <button className="btn">Move to Cart</button>
 
-              <button class="btn btn-tertiary">Remove from Wishlist</button>
+              <button className="btn btn-tertiary">Remove from Wishlist</button>
             </div>
           </div>
-          <div class="card ecom-card">
+          <div className="card ecom-card">
             <img src={productImage} alt="Product Image" />
 
-            <p class="wishlist-icon">
+            <p className="wishlist-icon">
               <ion-icon name="heart"></ion-icon>
             </p>
 
-            <p class="manufacturer">Demo Product</p>
+            <p className="manufacturer">Demo Product</p>
 
-            <p class="price">
+            <p className="price">
               ₹ 449
-              <span class="mrp">₹ 899</span>
-              <span class="discount">(49% off)</span>
+              <span className="mrp">₹ 899</span>
+              <span className="discount">(49% off)</span>
             </p>
 
-            <div class="action-container">
-              <button class="btn">Move to Cart</button>
+            <div className="action-container">
+              <button className="btn">Move to Cart</button>
 
-              <button class="btn btn-tertiary">Remove from Wishlist</button>
+              <button className="btn btn-tertiary">Remove from Wishlist</button>
             </div>
           </div>
-          <div class="card ecom-card">
+          <div className="card ecom-card">
             <img src={productImage} alt="Product Image" />
 
-            <p class="wishlist-icon">
+            <p className="wishlist-icon">
               <ion-icon name="heart"></ion-icon>
             </p>
 
-            <p class="manufacturer">Demo Product</p>
+            <p className="manufacturer">Demo Product</p>
 
-            <p class="price">
+            <p className="price">
               ₹ 449
-              <span class="mrp">₹ 899</span>
-              <span class="discount">(49% off)</span>
+              <span className="mrp">₹ 899</span>
+              <span className="discount">(49% off)</span>
             </p>
 
-            <div class="action-container">
-              <button class="btn">Move to Cart</button>
+            <div className="action-container">
+              <button className="btn">Move to Cart</button>
 
-              <button class="btn btn-tertiary">Remove from Wishlist</button>
+              <button className="btn btn-tertiary">Remove from Wishlist</button>
             </div>
           </div>
-          <div class="card ecom-card">
+          <div className="card ecom-card">
             <img src={productImage} alt="Product Image" />
 
-            <p class="wishlist-icon">
+            <p className="wishlist-icon">
               <ion-icon name="heart"></ion-icon>
             </p>
 
-            <p class="manufacturer">Demo Product</p>
+            <p className="manufacturer">Demo Product</p>
 
-            <p class="price">
+            <p className="price">
               ₹ 449
-              <span class="mrp">₹ 899</span>
-              <span class="discount">(49% off)</span>
+              <span className="mrp">₹ 899</span>
+              <span className="discount">(49% off)</span>
             </p>
 
-            <div class="action-container">
-              <button class="btn">Move to Cart</button>
+            <div className="action-container">
+              <button className="btn">Move to Cart</button>
 
-              <button class="btn btn-tertiary">Remove from Wishlist</button>
+              <button className="btn btn-tertiary">Remove from Wishlist</button>
             </div>
           </div>
         </section>
