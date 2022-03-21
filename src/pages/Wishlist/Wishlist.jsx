b1e2fb9e56a508fe8a315ce1593a048b27bcbca6
[Aman Jain] <[ajain8479@gmail.com]>
2022-03-22 02:14:27 +0530

    added all functionalities on wishlist page

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,123 +1,41 @@
 import React from "react";
 import Navbar from "../../components/NavBar/Navbar";
 import "./Wishlist.css";
-import productImage from "../../assets/images/product.webp";
+import emptyImage from "../../assets/images/empty-wish.svg";
+import { useData } from "../../contexts/Data-context";
+import { Link } from "react-router-dom";
+import Wishlistcard from "../../components/WishlistCard/Wishlistcard";
 
 const Wishlist = () => {
+  const { state, dispatch } = useData();
+  const wishList = state.products.filter((wish) => wish.wished);
+
   return (
     <div>
       <Navbar />
-      <h3 className="main-heading">My Wishlist</h3>
-      <main className="wishlist-main">
-        <section className="wishlist-product-menu">
-          <div className="card ecom-card">
-            <img src={productImage} alt="Product Image" />
+      <h3 className="main-heading">
+        My Wishlist ({state.wishlist.length} items)
+      </h3>
 
-            <p className="wishlist-icon">
-              <ion-icon name="heart"></ion-icon>
-            </p>
-
-            <p className="manufacturer">Demo Product</p>
-
-            <p className="price">
-              ₹ 449
-              <span className="mrp">₹ 899</span>
-              <span className="discount">(49% off)</span>
-            </p>
-
-            <div className="action-container">
-              <button className="btn">Move to Cart</button>
-
-              <button className="btn btn-tertiary">Remove from Wishlist</button>
-            </div>
-          </div>
-
-          <div className="card ecom-card">
-            <img src={productImage} alt="Product Image" />
-
-            <p className="wishlist-icon">
-              <ion-icon name="heart"></ion-icon>
-            </p>
-
-            <p className="manufacturer">Demo Product</p>
-
-            <p className="price">
-              ₹ 449
-              <span className="mrp">₹ 899</span>
-              <span className="discount">(49% off)</span>
-            </p>
-
-            <div className="action-container">
-              <button className="btn">Move to Cart</button>
-
-              <button className="btn btn-tertiary">Remove from Wishlist</button>
-            </div>
-          </div>
-          <div className="card ecom-card">
-            <img src={productImage} alt="Product Image" />
-
-            <p className="wishlist-icon">
-              <ion-icon name="heart"></ion-icon>
-            </p>
-
-            <p className="manufacturer">Demo Product</p>
-
-            <p className="price">
-              ₹ 449
-              <span className="mrp">₹ 899</span>
-              <span className="discount">(49% off)</span>
-            </p>
-
-            <div className="action-container">
-              <button className="btn">Move to Cart</button>
-
-              <button className="btn btn-tertiary">Remove from Wishlist</button>
-            </div>
-          </div>
-          <div className="card ecom-card">
-            <img src={productImage} alt="Product Image" />
-
-            <p className="wishlist-icon">
-              <ion-icon name="heart"></ion-icon>
-            </p>
-
-            <p className="manufacturer">Demo Product</p>
-
-            <p className="price">
-              ₹ 449
-              <span className="mrp">₹ 899</span>
-              <span className="discount">(49% off)</span>
-            </p>
-
-            <div className="action-container">
-              <button className="btn">Move to Cart</button>
-
-              <button className="btn btn-tertiary">Remove from Wishlist</button>
-            </div>
-          </div>
-          <div className="card ecom-card">
-            <img src={productImage} alt="Product Image" />
-
-            <p className="wishlist-icon">
-              <ion-icon name="heart"></ion-icon>
-            </p>
-
-            <p className="manufacturer">Demo Product</p>
-
-            <p className="price">
-              ₹ 449
-              <span className="mrp">₹ 899</span>
-              <span className="discount">(49% off)</span>
-            </p>
-
-            <div className="action-container">
-              <button className="btn">Move to Cart</button>
-
-              <button className="btn btn-tertiary">Remove from Wishlist</button>
-            </div>
-          </div>
-        </section>
-      </main>
+      {state.wishlist.length === 0 ? (
+        <div className="no-item">
+          <img className="empty-image" alt="empty wishlist" src={emptyImage} />
+          <span className="empty-content">
+            Have a look into our Product Collections{" "}
+          </span>
+          <Link to="/products" className="btn-logout">
+            Start Wishing
+          </Link>
+        </div>
+      ) : (
+        <main className="wishlist-main">
+          <section className="wishlist-product-menu">
+            {wishList.map((item) => (
+              <Wishlistcard key={item._id} wishItem={item} />
+            ))}
+          </section>
+        </main>
+      )}
     </div>
   );
 };
